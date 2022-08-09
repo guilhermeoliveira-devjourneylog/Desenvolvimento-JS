d1447f00a2857f01ca4559f9288338bedd1255e4
gui1hermeO1iveira <coder.guiliv@outlook.com>
2022-08-09 11:18:50 +0100

    Controller MVC 1-1
    'Script'
    - instância de negociação criada,
    - convertendo string data para um objeto Date.

--- bank-frame/client/js/app/controllers/NegociacaoController.js
+++ bank-frame/client/js/app/controllers/NegociacaoController.js
@@ -15,7 +15,33 @@ class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
     alert('Chamei ação no controller');
-    
+//verificando tipo de dado retornado por data.getTime
+    console.log(typeof(this._inputData.value));
+
+/* 1ª forma convertendo string data para um objeto date usando expressão regular
+    let data = new Date(this._inputData.value.replace(/-/g, ','));
+    console.log(data);
+*/
+
+// 2ª forma convertendo string data para um objeto date com paradigma funcional 
+    let data = new Date(...
+        this._inputData.value
+            .split('-')
+            .map((item, indice) => {
+              if(indice == 1) {
+                return item - 1;
+              }
+              return item;
+            })
+    );
+    console.log(data);
+//instância de negociacao
+    let negociacao = new Negociacao(
+      data,
+      this._inputQuantidade.value,
+      this._inputValor.value
+      );   
+    console.log(negociacao);
     console.log(this._inputData.value);
     console.log(this._inputQuantidade.value);
     console.log(this._inputValor.value);
